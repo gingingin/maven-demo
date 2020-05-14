034e63ffe79231070c7f4cfb5a26e22bdbdbacc8
hcylus <hercylus@gmail.com>
2020-05-14 22:38:10 +0800

    Update MyService.java

--- src/main/java/com/devops/MyService.java
+++ src/main/java/com/devops/MyService.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyService {
 
 	@RequestMapping("/")
+	LocalHostUtil.main()
+	/*	
 	public String hello()
 	{
 		return "Hello";
-	}
+	}*/
 	
 }
